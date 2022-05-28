2127d25c75407591a88c494d6712324e03d2a909
Josué David <josdavsm88@gmail.com>
2022-05-28 15:02:26 -0500

    employee dashboard content

---
+++ resources/views/employeeDashboard.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Employee Dshboard</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="header">
+		<div class="container">
+		<div class="btn-menu">
+			<label for="btn-menu">☰</label>
+		</div>
+			<div class="logo">
+				<h1>Employee Dashboard</h1>
+			</div>
+			<nav class="menu">
+				<a href="/">Home</a>
+			</nav>
+		</div>
+	</header>
+	<div class="capa"></div>
+
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+			<a href="/">1st Employee Action</a>
+			<a href="/">2nd Employee Action</a>
+			<a href="/">3rd Employee Action</a>
+			<a href="/">4th Employee Action</a>
+			<a href="/">5th Employee Action</a>
+			<a href="/">6th Employee Action</a>
+		</nav>
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+</body>
+</html>
