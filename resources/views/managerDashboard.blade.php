024b2b13c267655e07acecb13d541212f16f44b0
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-05-28 22:41:04 -0500

    Add manager dashboard table

--- resources/views/managerDashboard.blade.php
+++ resources/views/managerDashboard.blade.php
@@ -8,9 +8,9 @@
 <body>
 	<header class="header">
 		<div class="container">
-		<div class="btn-menu">
-			<label for="btn-menu">☰</label>
-		</div>
+		    <div class="btn-menu">
+			    <label for="btn-menu">☰</label>
+		    </div>
 			<div class="logo">
 				<h1>Manager Dashboard</h1>
 			</div>
@@ -20,20 +20,36 @@
 		</div>
 	</header>
 	<div class="capa"></div>
-
-<input type="checkbox" id="btn-menu">
-<div class="container-menu">
-	<div class="cont-menu">
-		<nav>
-			<a id="usersButton">Ver Usuarios</a>
-			<a id="packsButton">Ver Paquetes</a>
-			<a id="eventsButton">Ver Eventos</a>
-			<a id="paidsButton">Ver abonos</a>
-		</nav>
-		<label for="btn-menu">✖️</label>
-	</div>
-</div>
-<main id="main"></main>
-<script src="{{ asset('js/managerDashboard.js') }}"></script>
+    <input type="checkbox" id="btn-menu">
+    <div class="container-menu">
+	    <div class="cont-menu">
+		    <nav>
+		    	<a id="usersButton">Ver Usuarios</a>
+		    	<a id="packsButton">Ver Paquetes</a>
+		    	<a id="eventsButton">Ver Eventos</a>
+		    	<a id="paidsButton">Ver abonos</a>
+		    </nav>
+		    <label for="btn-menu">✖️</label>
+	    </div>
+    </div>
+    <main id="main">
+        <table class="table table-bordered table-striped" id="bookings">
+            <thead>
+                <tr>
+                    <th>id evento</th>
+                    <th>Fecha evento</th>
+                    <th>precio</th>
+                    <th>Usuario</th>
+                    <th>Confirmado</th>
+                    <th>Actualizar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+        </table>
+    </main>
+    <script src="{{ asset('js/managerDashboard.js') }}"></script>
 </body>
 </html>
