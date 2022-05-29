fe56f62884ed69cc8a7febdd6d871fd6d5f504f1
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-05-28 21:33:20 -0500

    AJAX events query

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index(){
+        $events = Event::all();
+        $events->toJson();
+        return $events;
+    }
 }
--- public/js/managerDashboard.js
+++ public/js/managerDashboard.js
@@ -3,30 +3,36 @@
             $usersButton = document.getElementById('usersButton'),
             $packsButton = document.getElementById('packsButton'),
             $eventsButton = document.getElementById('eventsButton'),
-            $paidsButton = document.getElementById('paidsButton')
+            $paidsButton = document.getElementById('paidsButton'),
             $fragment = document.createDocumentFragment();
     
-        $usersButton.addEventListener('click', () => {
-            fetch('/manager/users')
-                .then((res) => res.ok ? res.json() : Promise.reject(res))
-                .then((users) => {
-                    console.log(users);
-                }).catch((err) => {
-                    console.error(`Error ${err.status}: ${err.statusText}`);
-                    $main.innerHTML = `Error ${err.status}: ${err.statusText}`
-                })
-            console.log('users');
-        })
+    $usersButton.addEventListener('click', () => {
+        fetch('/manager/users')
+            .then((res) => res.ok ? res.json() : Promise.reject(res))
+            .then((users) => {
+                console.log(users);
+            }).catch((err) => {
+                console.error(`Error ${err.status}: ${err.statusText}`);
+                $main.innerHTML = `Error ${err.status}: ${err.statusText}`
+            })
+    })
 
-        $packsButton.addEventListener('click', () => {
-            console.log('packs');
-        })
+    $packsButton.addEventListener('click', () => {
+        console.log('packs');
+    })
 
-        $eventsButton.addEventListener('click', () => {
-            console.log('events');
-        })
+    $eventsButton.addEventListener('click', () => {
+        fetch('/manager/events')
+            .then((res) => res.ok ? res.json() : Promise.reject(res))
+            .then((events) => {
+                console.log(events);
+            }).catch((err) => {
+                console.error(`Error ${err.status}: ${err.statusText}`);
+                $main.innerHTML = `Error ${err.status}: ${err.statusText}`
+            })
+    })
 
-        $paidsButton.addEventListener('click', () => {
-            console.log('paids');
-        })
+    $paidsButton.addEventListener('click', () => {
+        console.log('paids');
+    })
 })()
